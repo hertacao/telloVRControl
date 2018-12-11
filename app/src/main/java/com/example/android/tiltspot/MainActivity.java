d41db6e3c561fa3cde79863f52df2d5ea4c80cac
atom632 <6fqrPbSgNtAiBbmvNtG3>
2018-12-11 23:16:30 +0800

    basic functionality without network

--- app/src/main/java/com/example/android/tiltspot/MainActivity.java
+++ app/src/main/java/com/example/android/tiltspot/MainActivity.java
@@ -26,43 +26,72 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import static android.hardware.Sensor.TYPE_GYROSCOPE;
-
-public class MainActivity extends AppCompatActivity
-        implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     // System sensor manager instance.
     private SensorManager mSensorManager;
 
-    // Accelerometer and magnetometer sensors, as retrieved from the
-    // sensor manager.
+    // Accelerometer and magnetometer sensors, as retrieved from the sensor manager.
     private Sensor mSensorAccelerometer;
     private Sensor mSensorMagnetometer;
-    private Sensor mGyroscope; ///////////////
     private String sensorName;
 
     private float[] mAccelerometerData = new float[3];
     private float[] mMagnetometerData = new float[3];
+    private float azimuthOffset;
+    private float azimuth;
+    private float pitch;
+    private float roll;
 
+    private boolean start = true;
+    private State state;
 
-    // TextViews to display current sensor values.
     private TextView mTextSensorAzimuth;
     private TextView mTextSensorPitch;
     private TextView mTextSensorRoll;
     private TextView mTextSensor;
 
+    private UAVInteraction drone;
+    private ConnErrorDialog connErrorMessage;
+
     // Very small values for the accelerometer (on all three axes) should
     // be interpreted as 0. This value is the amount of acceptable
     // non-zero drift.
     private static final float VALUE_DRIFT = 0.05f;
+    private static final float HYSTERESIS = 0.05f;
+    private static final float OFFSETFROMMIDDLE = 0.2f;
+
+    protected enum State {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT,
+        ROTATELEFT,
+        ROTATERIGHT,
+        HOVER
+    }
+
+    //methods
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        try {
+            drone = new UAVInteraction();
+            state = State.HOVER;
+        } catch (Exception e) {
+            //idea of this is to close the app if we dont have connection
+            connErrorMessage = new ConnErrorDialog();
+            //TO DO:    close app
+        }
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Lock the orientation to portrait (for now)
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Lock the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         mTextSensorAzimuth = (TextView) findViewById(R.id.value_azimuth);
         mTextSensorPitch = (TextView) findViewById(R.id.value_pitch);
@@ -80,8 +109,9 @@ public class MainActivity extends AppCompatActivity
         mSensorMagnetometer = mSensorManager.getDefaultSensor(
                 Sensor.TYPE_MAGNETIC_FIELD);
 
-        mGyroscope = mSensorManager.getDefaultSensor(TYPE_GYROSCOPE);
+        //mGyroscope = mSensorManager.getDefaultSensor(TYPE_GYROSCOPE);
     }
+
 
     /**
      * Listeners for the sensors are registered in this callback so that
@@ -90,7 +120,6 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onStart() {
         super.onStart();
-
         // Listeners for the sensors are registered in this callback and
         // can be unregistered in onStop().
         //
@@ -105,10 +134,19 @@ public class MainActivity extends AppCompatActivity
             mSensorManager.registerListener(this, mSensorMagnetometer,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
+
+        //TO DO: minimalize the app
+
+
     }
 
     @Override
     protected void onStop() {
+
+        //====== LAND THE UAV ======
+
+        drone.land();
+
         super.onStop();
 
         // Unregister all sensor listeners in this callback so they don't
@@ -120,9 +158,7 @@ public class MainActivity extends AppCompatActivity
     public void onSensorChanged(SensorEvent sensorEvent) {
 
         int sensorType = sensorEvent.sensor.getType();
-        sensorName = sensorEvent.sensor.getName();
-
-
+        //sensorName = sensorEvent.sensor.getName();
 
         switch (sensorType) {
             case Sensor.TYPE_ACCELEROMETER:
@@ -132,7 +168,7 @@ public class MainActivity extends AppCompatActivity
                 mMagnetometerData = sensorEvent.values.clone();
                 break;
             default:
-                mTextSensor.setText(sensorName);
+                //mTextSensor.setText(sensorName);
                 return;
         }
 
@@ -145,53 +181,84 @@ public class MainActivity extends AppCompatActivity
             SensorManager.getOrientation(rotationMatrix, orientationValues);
         }
 
-        float azimuth = orientationValues[0];
-        float pitch = orientationValues[1];
-        float roll = orientationValues[2];
-
-        mTextSensorAzimuth.setText(getResources().getString(
-                R.string.value_format, azimuth));
-        mTextSensorPitch.setText(getResources().getString(
-                R.string.value_format, pitch));
-        mTextSensorRoll.setText(getResources().getString(
-                R.string.value_format, roll));
-
-        if(Math.abs(roll) < 1.5){
-            mTextSensor.setText("forward");
-            //
-            //call method to go forward
-        }else if(Math.abs(roll) > 1.9){
-            mTextSensor.setText("backward");
-        }else{
-            mTextSensor.setText("stop");
+        if (start) {
+            azimuthOffset = orientationValues[0];
+            start = false;
         }
 
+        azimuth = orientationValues[0] - azimuthOffset;
+        pitch = orientationValues[1];
+        roll = Math.abs(orientationValues[2]);
+
+        mTextSensorAzimuth.setText(getResources().getString(R.string.value_format, azimuth));
+        mTextSensorPitch.setText(getResources().getString(R.string.value_format, pitch));
+        mTextSensorRoll.setText(getResources().getString(R.string.value_format, roll));
 
 
+        //PITCH 0,3
 
+        processData();
+    }
 
+    private void processData() {
+        //TO DO: yet to implement rotation
+        if (state == State.HOVER) {
+            if (roll < (1.57 - OFFSETFROMMIDDLE)) {
+                drone.forward();
+                mTextSensor.setText("forward");
+                state = State.FORWARD;
 
+            } else if (roll > (1.57 + OFFSETFROMMIDDLE)) {
+                drone.backward();
+                mTextSensor.setText("backward");
+                state = State.BACKWARD;
 
+            } else if (pitch < -OFFSETFROMMIDDLE) {
+                drone.right();
+                mTextSensor.setText("right");
+                state = State.RIGHT;
 
+            } else if (pitch > OFFSETFROMMIDDLE) {
+                drone.left();
+                mTextSensor.setText("left");
+                state = State.LEFT;
 
+            } else {
+                drone.hover();
+                mTextSensor.setText("hover");
+                state = State.HOVER;
 
+            }
+        } else { //HYSTERESIS
 
+            if (roll < (1.57 - OFFSETFROMMIDDLE - HYSTERESIS)) {
+                drone.forward();
+                mTextSensor.setText("forward");
+                state = State.FORWARD;
 
+            } else if (roll > (1.57 + OFFSETFROMMIDDLE - HYSTERESIS)) {
+                drone.backward();
+                mTextSensor.setText("backward");
+                state = State.BACKWARD;
 
+            } else if (pitch < (-OFFSETFROMMIDDLE - HYSTERESIS)) {
+                drone.right();
+                mTextSensor.setText("right");
+                state = State.RIGHT;
 
-        //Log.d(sensorName + ": X: " + sensorEvent.values[0] + "; Y: " + sensorEvent.values[1] + "; Z: " + sensorEvent.values[2] + ";");
-//        float azimuth = sensorEvent.values[0];
-//        String.format(".1f", azimuth);
-//        mTextSensorAzimuth.setText(String.format("%2.2f", azimuth));
-//
-//        float roll = sensorEvent.values[1];
-//        String.format(".1f", roll);
-//        mTextSensorRoll.setText(String.format("%2.2f", roll));
-//
-//
-//        float pitch = sensorEvent.values[2];
-//        String.format(".1f", pitch);
-//        mTextSensorPitch.setText(String.format("%2.2f", pitch));
+            } else if (pitch > (OFFSETFROMMIDDLE - HYSTERESIS)) {
+                drone.left();
+                mTextSensor.setText("left");
+                state = State.LEFT;
+
+            }else{
+                drone.hover();
+                mTextSensor.setText("hover");
+                state = State.HOVER;
+            }
+
+        }
+
     }
 
     /**
---
+++ app/src/main/java/com/example/android/tiltspot/UAVInteraction.java
@@ -0,0 +1,27 @@
+package com.example.android.tiltspot;
+
+public class UAVInteraction {
+
+
+    public void land() {
+    }
+
+    public void UAVInteraction(){
+        //setup connection with a drone, if failed, should throw exception
+    }
+
+    public void forward() {
+    }
+
+    public void backward() {
+    }
+
+    public void right() {
+    }
+
+    public void left() {
+    }
+
+    public void hover() {
+    }
+}
